00b63c5399a7890ae2db06045045c663e3eba89d
samueldlh <samueldlh.dev@gmail.com>
2021-01-17 07:55:02 -0500

    Angular material, header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ProductComponent } from './components/product/product.component';
+///
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon'; 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    ProductComponent
-  ],
+  declarations: [AppComponent, HeaderComponent, ProductComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/models/Product.ts
+++ src/app/models/Product.ts
@@ -1,3 +1,5 @@
 export class Product{
-    
+    id: number = 0;
+    name: string = '';
+    description: string = '';
 }
